e3992e7d2e3e807924dbdc88ae46be1144a64812
Juan Cruz-Benito <juan.cruz.benito@ibm.com>
2025-09-02 16:52:12 -0600

    Fixing the path for styles /CSS files in PR previews

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,14 @@ autodoc_mock_imports = ['torch', 'numpy', 'loguru']
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Configure base URL for GitHub Pages PR previews
+import os
+if os.environ.get('GITHUB_EVENT_NAME') == 'pull_request' and os.environ.get('PR_NUMBER'):
+    pr_number = os.environ.get('PR_NUMBER')
+    html_baseurl = f'/twisteRL/pr-{pr_number}/'
+else:
+    html_baseurl = '/twisteRL/'
 html_logo = '../assets/twisterl-logo.png'
 html_favicon = '../assets/twisterl-logo.png'
 
